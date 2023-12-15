6bb29afc2648e809e19077df2fee45b46b6e0c29
Thiago Macedo <1750447+macedot@users.noreply.github.com>
2023-12-16 00:38:54 +0100

    2013: day 10 (c++) - first part

--- 2023/c++/10.cpp
+++ 2023/c++/10.cpp
@@ -1,5 +1,11 @@
 #include "aoc.hpp"
 
+struct ELEM {
+    PVALUE pos;
+    VALUE  value;
+};
+using QUEUE = std::deque<ELEM>;
+
 PVALUE findStart(VSTRING const& M)
 {
     PVALUE start;
@@ -14,95 +20,90 @@ PVALUE findStart(VSTRING const& M)
     throw std::runtime_error("Invalid input!");
 }
 
-VALUE walkMaze(VSTRING const& M, MVPAIR& V, DPAIR& Q, VALUE atu)
+VALUE walkMaze(VSTRING const& M, PVALUE start)
 {
-    return atu;
-    // const auto& [j, i] = pos;
-    // if (j < 0 || j >= M.size()) {
-    //     return atu;
-    // }
-    // if (i < 0 || i >= M[j].size()) {
-    //     return atu;
-    // }
-    // if (V.contains(pos)) {
-    //     if (V.at(pos) < atu + 1) {
-    //         return atu;
-    //     }
-    // }
+    const auto& [j, i] = start;
 
-    // atu++;
-    // V.insert(pos);
-    // for (const auto& p : V) {
-    //     fmt::print("({},{}),", p.first, p.second);
-    // }
-    // fmt::print(" -> {},{},{}\n", j, i, atu);
-
-    // const auto X   = M[j][i];
-    // const auto res = [&]() {
-    //     switch (X) {
-    //         case '|':
-    //             return std::max(walkMaze(M, V, P(j - 1, i), atu),
-    //                             walkMaze(M, V, P(j + 1, i), atu));
-    //         case '-':
-    //             return std::max(walkMaze(M, V, P(j, i - 1), atu),
-    //                             walkMaze(M, V, P(j, i + 1), atu));
-    //         case 'L':
-    //             return std::max(walkMaze(M, V, P(j - 1, i), atu),
-    //                             walkMaze(M, V, P(j, i + 1), atu));
-    //         case 'J':
-    //             return std::max(walkMaze(M, V, P(j - 1, i), atu),
-    //                             walkMaze(M, V, P(j, i - 1), atu));
-    //         case '7':
-    //             return std::max(walkMaze(M, V, P(j + 1, i), atu),
-    //                             walkMaze(M, V, P(j, i - 1), atu));
-    //         case 'F':
-    //             return std::max(walkMaze(M, V, P(j + 1, i), atu),
-    //                             walkMaze(M, V, P(j, i + 1), atu));
-    //     }
-    //     //case '.':
-    //     return atu - 1;
-    // }();
-
-    // return std::max(atu, res);
-}
-
-VALUE walkStart(VSTRING const& M, PVALUE pos)
-{
-    const auto& [j, i] = pos;
-
-    MVPAIR V;
-    V[pos] = 0;
-
-    DPAIR Q;
+    QUEUE Q;
     if (j > 0) {
         const auto N = M[j - 1][i];
         if (N == '|' || N == 'F' || N == '7') {
-            Q.emplace_back(P(j - 1, i));
+            Q.emplace_back(ELEM{ P(j - 1, i), 1 });
         }
     }
 
     if (j + 1 < M.size()) {
         const auto S = M[j + 1][i];
         if (S == '|' || S == 'L' || S == 'J') {
-            Q.emplace_back(P(j + 1, i));
+            Q.emplace_back(ELEM{ P(j + 1, i), 1 });
         }
     }
 
     if (i > 0) {
         const auto W = M[j][i - 1];
         if (W == '-' || W == 'F' || W == 'L') {
-            Q.emplace_back(P(j, i - 1));
+            Q.emplace_back(ELEM{ P(j, i - 1), 1 });
         }
     }
 
     if (i + 1 < M[j].size()) {
         const auto E = M[j][i + 1];
         if (E == '-' || E == 'J' || E == '7') {
-            Q.emplace_back(P(j, i + 1));
+            Q.emplace_back(ELEM{ P(j, i + 1), 1 });
         }
     }
 
-    return walkMaze(M, V, Q, 0);
+    SPAIR V;
+    V.insert(start);
+
+    VALUE res = 0;
+    while (!Q.empty()) {
+        const auto elem = Q.front();
+        Q.pop_front();
+
+        const auto& [j, i] = elem.pos;
+        if (j < 0 || j >= M.size()) {
+            continue;
+        }
+        if (i < 0 || i >= M[j].size()) {
+            continue;
+        }
+        if (V.contains(elem.pos)) {
+            continue;
+        }
+
+        V.insert(elem.pos);
+        res = std::max(res, elem.value);
+
+        switch (M[j][i]) {
+            case '|':
+                Q.emplace_back(ELEM{ P(j - 1, i), elem.value + 1 });
+                Q.emplace_back(ELEM{ P(j + 1, i), elem.value + 1 });
+                break;
+            case '-':
+                Q.emplace_back(ELEM{ P(j, i - 1), elem.value + 1 });
+                Q.emplace_back(ELEM{ P(j, i + 1), elem.value + 1 });
+                break;
+            case 'L':
+                Q.emplace_back(ELEM{ P(j - 1, i), elem.value + 1 });
+                Q.emplace_back(ELEM{ P(j, i + 1), elem.value + 1 });
+                break;
+            case 'J':
+                Q.emplace_back(ELEM{ P(j - 1, i), elem.value + 1 });
+                Q.emplace_back(ELEM{ P(j, i - 1), elem.value + 1 });
+                break;
+            case '7':
+                Q.emplace_back(ELEM{ P(j + 1, i), elem.value + 1 });
+                Q.emplace_back(ELEM{ P(j, i - 1), elem.value + 1 });
+                break;
+            case 'F':
+                Q.emplace_back(ELEM{ P(j + 1, i), elem.value + 1 });
+                Q.emplace_back(ELEM{ P(j, i + 1), elem.value + 1 });
+                break;
+        }
+    }
+
+    return res;
 }
 
 int main(int argc, char* argv[])
@@ -113,7 +114,7 @@ int main(int argc, char* argv[])
 
     const auto grid  = readFile(argv[1]);
     const auto start = findStart(grid);
-    const auto ans1  = walkStart(grid, start);
+    const auto ans1  = walkMaze(grid, start);
     fmt::print("{}\n", ans1);
 
     return 0;
